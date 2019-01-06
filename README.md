557cedcbb003cc5238c8a25b889f0f004685b364
fast-cache <2598187219@qq.com>
2019-01-06 16:40:04 +0800

    full text

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # fast-cache
 短小精悍的前端缓存项目，防止内存泄漏
 hello.ablert
+
+## 简介
+
+## 快速使用
+
+简单接入文档
+
+- 使用文档
+- 二次开发文档
+
+## 交流 & 提问
+https://github.com/ablert952/fast-cache/issues
---
+++ webpack.dev.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: './release/bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './example/index.html'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './release'),
+        open: true,
+        port: 9000
+    }
+}
